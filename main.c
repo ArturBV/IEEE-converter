505ba79a02771a8bc2728ddb183117e50affae37
ArturBV <yablokco44@gmail.com>
2022-08-07 22:01:41 +0500

    version 0.1 done

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <math.h>
 
-/* constants are here */
+/* some constants are here */
 #include "files/parameter_handler.h"
 #include "files/input_handler.h"
 
@@ -35,45 +35,37 @@ void add_bit(char* number, int index) {
 	return;
 }
 
-char* strreverse(char* str, int len) {
-	char tmp;
-
-	for (int i = 0; i < len / 2; i++) {
-		tmp = str[i];
-		str[i] = str[len - i - 1];
-		str[len - i - 1] = tmp;
-	}
-
-	return str;
-}
-
-/* denominator > 1 */
+/* reduce frac before call this function */
 /* number must be betwen 0 and 1 */
 char* binary_notion(Fraction* number, int precision) {
 	int i;
-	int higher_bit_index;
 	int compare_result, need_round = 1;
 	int tmp_value, tmp_bin_len, is_integer;
 	char *binary_str = (char*) calloc(precision + ROUND_BITS + 1, sizeof(char));
 	char *round_bits = (char*) calloc(ROUND_BITS + 1, sizeof(char));
+	
+	Fraction *number_cp = (Fraction*) calloc(1, sizeof(Fraction));
+	number_cp->numerator = number->numerator;
+	number_cp->denominator = number->denominator;
 
-	if (number->denominator != 1) {
-		tmp_value = number->denominator;
+	if (number_cp->denominator != 1) {
+		tmp_value = number_cp->denominator;
 		is_integer = 0;
 	} else {
-		tmp_value = number->numerator;
+		tmp_value = number_cp->numerator;
 		is_integer = 1;
 
-		/* get higher bit of tmp_value (indexation from left) */
+		/* get higher bit of tmp_value (indexation from left) and store in `i` */
 		/* why not take a solution from Hacker's Delight? */
 		/* nah, too easy */
 		for (i = 0; !((tmp_value >> (BITS_IN_INT - i - 1)) & 1) &&
 		        i < BITS_IN_INT; i++) {}
 
-		higher_bit_index = i;
-		tmp_bin_len = BITS_IN_INT - higher_bit_index;
+		/* len of tmp_value in binary notation without leading zeros */
+		tmp_bin_len = BITS_IN_INT - i;
 
 		if (tmp_bin_len <= precision) {
+			/* get binary str */
 			for (i = 0; i < precision - tmp_bin_len; i++) {
 				binary_str[i] = '0';
 			}
@@ -86,9 +78,8 @@ char* binary_notion(Fraction* number, int precision) {
 	}
 
 	for (i = 0; i < precision + ROUND_BITS; i++) {
-
 		/* end of converting */
-		if ((i + 1 > tmp_bin_len && is_integer) || (number->denominator == 1 && !is_integer)) {
+		if ((i + 1 > tmp_bin_len && is_integer) || (number_cp->denominator == 1 && !is_integer)) {
 			while (i < precision + ROUND_BITS) {
 				binary_str[i++] = '0';
 			}
@@ -97,11 +88,11 @@ char* binary_notion(Fraction* number, int precision) {
 
 		/* main operation */
 		if (!is_integer) {
-			/* int overflow? */
-			number->numerator *= 2;
-			if (number->numerator / number->denominator) {
+			/* int overflow? wtf why?*/
+			number_cp->numerator *= 2;
+			if (number_cp->numerator / number_cp->denominator) {
 				binary_str[i] = '1';
-				number->numerator -= number->denominator;
+				number_cp->numerator -= number_cp->denominator;
 			} else {
 				binary_str[i] = '0';
 			}
@@ -129,6 +120,7 @@ char* binary_notion(Fraction* number, int precision) {
 		}
 	}
 
+	free(number_cp);
 	free(round_bits);
 	binary_str[precision] = '\0';
 	return binary_str;
@@ -186,6 +178,8 @@ int main(void) {
 		printf("Error! Division by zero!\n");
 		return 1;
 	}
+	/* IEEE notation:   (-1)^s * m * 2^e */
+	/* binary notation:	|s||  exp_str  ||  frac_str  | */
 	/* s */
 	if (num->numerator < 0) {
 		s = '1';
@@ -195,8 +189,7 @@ int main(void) {
 	}
 	reduce_frac(num);
 
-
-	long long exp_shift;
+	int exp_shift;
 	exp_shift = (1 << (exp_n - 1)) - 1;
 
 	int number_type;
@@ -209,27 +202,20 @@ int main(void) {
 		.numerator = frac_step - 1,
 		.denominator = min_denormal.denominator
 	};
-	
+
 	if (cmp_abs_fraction(num, &max_denormal) <= 0) {
 		number_type = DENORMAL;
 	} else {
 		number_type = NORMAL;
 	}
 
-	printf("exp_n:%d, frac_n:%d, exp_shift:%lld\n", exp_n, frac_n, exp_shift);
-	printf("%d %d, %d %d max:%d/%d\n", num->numerator, num->denominator,
-	       max_denormal.denominator, frac_step,
-	       max_denormal.numerator, max_denormal.denominator);
-	/* IEEE notation:   (-1)^s * m * 2^e */
-	/* binary notation:	|s||  exp_str  ||  frac_str  | */
-	char *exp_str, *frac_str;
+	char *exp_str, *frac_str, *full_str;
 	int exp_i;
 	int e_power = 0;
 	Fraction exp_f = {
 		.numerator = 0,
 		.denominator = 1
 	};
-
 
 	/* exp_i */
 	if (number_type == NORMAL) {
@@ -241,7 +227,7 @@ int main(void) {
 			}
 			num->numerator -= num->denominator;
 			reduce_frac(num);
-		} else if (num->numerator < num->denominator) {
+		} else {
 			while (num->numerator < num->denominator) {
 				num->numerator *= 2;
 				reduce_frac(num);
@@ -257,24 +243,15 @@ int main(void) {
 		reduce_frac(num);
 	}
 
-	exp_i = (int) exp_shift + e_power;
+	exp_i = exp_shift + e_power;
 	exp_f.numerator = exp_i;
 	exp_str = binary_notion(&exp_f, exp_n);
 
 	/* frac */
 	frac_str = binary_notion(num, frac_n);
+	full_str = binary_notion(num, frac_n + ROUND_BITS * 2);
 
-	// /* not a frac case */
-	// if (num->denominator == 1) {
-
-	// 	/* frac */
-	// 	num->denominator = 1 << e_power; /* 2^e_power */
-	// 	num->numerator -= num->denominator; /* 0 <= num <= 1 */
-
-	// 	frac_str = binary_notion(num, frac_n);
-	// }
-
-	printf("Result: %c %s %s\n", s, exp_str, frac_str);
+	printf("Result: %c %s %s\n\n", s, exp_str, frac_str);
 
 	if (number_type == NORMAL) {
 		printf("NORMAL\n");
@@ -282,13 +259,12 @@ int main(void) {
 		printf("DENORMAL\n");
 	}
 
-	printf("exp_i:%d\n", exp_i);
-	// printf("%f\n", pow(2, e_power));
-	printf("exp_shift:%lld\n", exp_shift);
+	printf("exp_i:%d (converts into binary)\n", exp_i);
+	printf("exp_shift:%d\n", exp_shift);
 	printf("e_power:%d\n", e_power);
-	// printf("%d %d\n", exp_n, frac_n);
-	printf("%d / %d\n", num->numerator, num->denominator);
+	printf("frac before round:%s\n", full_str);
 
+	free(full_str);
 	free(exp_str);
 	free(frac_str);
 	free(num);
